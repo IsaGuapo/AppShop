e915f26b735f651541909561a99bea8ef7d367c5
IsaGuapo <isabela.guapo@gmail.com>
2022-01-05 16:32:35 +0100

    pagination and limit products

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -5,7 +5,7 @@ const Fab = require('../models/fabModels')
 // CONTROLLERS
 
 // Get all products with QueryParams
-//Filtrado
+//FILTRADO
 exports.getAllProducts = async (req, res) => {
   try {
     //Construccion de la copia de la query para filtrar por los parametros que queremos
@@ -14,7 +14,7 @@ exports.getAllProducts = async (req, res) => {
   
     excludeParams.forEach(e => delete copyQuery[e]);//bucle de un array para que devuelva cada uno de los elementos//con esta funcion borro cada uno de los elementos que coincidan con los que estoy excluyendo en excludeParams
     
-    //Filtrado avanzado
+    //FFILTRADO AVANZADO
     //[gt]  <  mayor que
     //[gte] <= mayor o igual (palabra reservada de Mongo)
     //[lte] >= menor o igual  -- hay que añadir un simbolo de  $ para poder hacer estas busquedas en mongo
@@ -22,10 +22,40 @@ exports.getAllProducts = async (req, res) => {
     let stringQuery = JSON.stringify(copyQuery); //Paso a string el objeto para poder utilizar regex y añadir simbolo $
     stringQuery = stringQuery.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); //funcion para añadir $
       console.log (stringQuery)
-    let  query = Product.find(JSON.parse(stringQuery)) //con Find() metodo de mongoose. buscamos todos los productos filtrado previamente por los parametros que le he dicho en mi copyQuery
+    
+      let  query = Product.find(JSON.parse(stringQuery)) //con Find() metodo de mongoose. buscamos todos los productos filtrado previamente por los parametros que le he dicho en mi copyQuery
     //con JSON.parse convierto el string a objeto de nuevo.
 
-  //ejecuto las querys
+
+    //MÉTODO DE ORDENACIÓN
+    if(req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    }else {
+      query =query.sort ('-price') //ordena de forma ascendente al poner -
+    }
+    
+
+    //PAGINACIÓN
+    //skip es la cantidad de resultados que van a ser omitidos
+    //limit nos dice cuantos resultados queremos que nos muestre
+
+    //página por defecto
+    const page = req.query.page * 1 || 1 ;
+    const limit1 = req.query.limit * 1 || 10; //el usuario podra elegir el limte de productos
+    const skip1 = (page-1) * limit1;
+
+    query = query.skip(skip1).limit(limit1)
+
+  //con este if devuelvo un mensaje si no existe la pagina
+    if(req.query.page){
+      const numProducts = await Product.countDocuments();
+      if (skip1 >= numProducts) {
+        throw new Error ('This page doesn´t exist')}
+    }
+
+
+  //EJECUCIÓN DE QUERYS
     const products = await query.populate('fab'); //con populate() metodo de mongoose añadimos tb los datos guardados en fabricante(fab)
     res.status(200).json({
       status: 'success',
